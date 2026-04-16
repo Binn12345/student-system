cd0d81bd9be314c76045809a5886eee0dd7753f0
Binn12345 <arvincejalbo18@gmail>
2026-04-16 17:17:57 +0800

    adding modal confirmation before logout

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -24,6 +24,33 @@ if (!isset($_SESSION['user_id'])) {
 
 <body>
 
+<div class="modal fade" id="logoutModal" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+
+      <div class="modal-header">
+        <h5 class="modal-title">Confirm Logout</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+      </div>
+
+      <div class="modal-body">
+        Are you sure you want to sign out?
+      </div>
+
+      <div class="modal-footer">
+        <!-- <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+          Cancel
+        </button> -->
+
+        <a href="../logout.php" class="btn btn-danger">
+          Yes, Logout
+        </a>
+      </div>
+
+    </div>
+  </div>
+</div>
+
   <!-- ======= Header ======= -->
   <header id="header" class="header fixed-top d-flex align-items-center">
 
@@ -238,11 +265,18 @@ if (!isset($_SESSION['user_id'])) {
               <hr class="dropdown-divider">
             </li>
 
-            <li>
+            <!-- <li>
               <a class="dropdown-item d-flex align-items-center" href="../logout.php">
                 <i class="bi bi-box-arrow-right"></i>
                 <span>Sign Out</span>
               </a>
+            </li> -->
+
+            <li>
+            <a class="dropdown-item d-flex align-items-center" href="#" data-bs-toggle="modal" data-bs-target="#logoutModal">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+            </a>
             </li>
 
           </ul><!-- End Profile Dropdown Items -->
